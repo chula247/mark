d3aeb5cf7bd329b597e97f5654cf761dce7d367a
chula247 <chulabaraka12148@gmail.com>
2025-02-05 13:01:23 +0300

    initial commit

---
+++ assignment.py
@@ -0,0 +1,6 @@
+# A progam to check whether a year is a leap year
+
+
+
+
+# A program to check whether a letter is a consonant or a vowel
---
+++ controlstatements.py
@@ -0,0 +1,29 @@
+
+temperature = 45
+
+if temperature > 25 :
+    print("It is too hot")
+else :
+    print("It is too cold")
+
+# A program that returns the smallest number
+first = 60
+second = 90
+third = 20
+
+
+if first < second and first >third :
+        print(first,"Is the smallest number")
+elif second< first and second < third :
+    print(second,"is the smallest number")
+else :
+    print(third,"is the smallest number")
+
+# A python program to check whether a number is even or odd
+number = 0
+if number ==0:
+    print(number,"is a neutral number")
+if number % 2 == 0:
+    print(number,"is an even number")
+else:
+    print(number,"is an odd number")
